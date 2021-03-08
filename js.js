cbff362dcb506de639eed533ca9af95d67d527b9
Max <39224167+Fenzdy@users.noreply.github.com>
2021-03-08 09:58:45 +0300

    wqihjgb

--- js.js
+++ js.js
@@ -1,10 +1,11 @@
-msgData1 = $('.start-time').text();
-    msgData2 = $('.end-time').text();
-    msgData3 = $('.Location').text();
+// msgData1 = $('.start-time').text();
+//     msgData2 = $('.end-time').text();
+//     msgData3 = $('.Location').text();
 
-    var icsMSG = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Our Company//NONSGML v1.0//EN\nBEGIN:VEVENT\nUID:me@google.com\nDTSTAMP:20120315T170000Z\nATTENDEE;CN=My Self ;RSVP=TRUE:MAILTO:me@gmail.com\nORGANIZER;CN=Me:MAILTO::me@gmail.com\nDTSTART:" + msgData1 +"\nDTEND:" + msgData2 +"\nLOCATION:" + msgData3 + "\nSUMMARY:Our Meeting Office\nEND:VEVENT\nEND:VCALENDAR";
+//     var icsMSG = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Our Company//NONSGML v1.0//EN\nBEGIN:VEVENT\nUID:me@google.com\nDTSTAMP:20120315T170000Z\nATTENDEE;CN=My Self ;RSVP=TRUE:MAILTO:me@gmail.com\nORGANIZER;CN=Me:MAILTO::me@gmail.com\nDTSTART:" + msgData1 +"\nDTEND:" + msgData2 +"\nLOCATION:" + msgData3 + "\nSUMMARY:Our Meeting Office\nEND:VEVENT\nEND:VCALENDAR";
 
-    $('.test').click(function(){
-        window.open( "data:text/calendar;charset=utf8," + escape(icsMSG));
-        // console.log('asdasd');
-    });
+//     $('.test').click(function(){
+//         // window.open( "data:text/calendar;charset=utf8," + icsMSG);
+//         console.log(location.href = "webcal:data:text/calendar;charset=utf8," + icsMSG);
+//         // location.href = "data:text/calendar;charset=utf8," + icsMSG;
+//     });
